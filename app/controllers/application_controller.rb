b23a5670a3c0c08edf2df247f477b5a43ce51d54
yokoi.shinya <shinya.yokoi@gmail.com>
2015-04-24 15:19:23 +0900

    access-control-allow-origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
 
   after_filter :set_access_control_headers
  
-　def set_access_control_headers 
-　　headers['Access-Control-Allow-Origin'] = '*' 
-　　headers['Access-Control-Request-Method'] = '*' 
+def set_access_control_headers 
+headers['Access-Control-Allow-Origin'] = '*' 
+headers['Access-Control-Request-Method'] = '*' 
   end
 end
