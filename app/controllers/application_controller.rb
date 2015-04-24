b9a9db79f2ab0d4b1ff8cfa2cc814cefebb5c3a4
yokoi.shinya <shinya.yokoi@gmail.com>
2015-04-24 15:09:59 +0900

    access-control-allow-origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
    # ------ ここから下が追加した設定です　-----------------
-  before_filter :allow_cross_domain_access
-  def allow_cross_domain_access
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Methods"] = "PUT,DELETE,POST,GET,OPTIONS"
+  #before_filter :allow_cross_domain_access
+  #def allow_cross_domain_access
+  #  response.headers["Access-Control-Allow-Origin"] = "*"
+  #  response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+  #  response.headers["Access-Control-Allow-Methods"] = "PUT,DELETE,POST,GET,OPTIONS"
+  #end
+
+  protect_from_forgery with: :exception
+  after_filter :set_access_control_headers
+ 
+　def set_access_control_headers 
+　　headers['Access-Control-Allow-Origin'] = '*' 
+　　headers['Access-Control-Request-Method'] = '*' 
   end
 end
